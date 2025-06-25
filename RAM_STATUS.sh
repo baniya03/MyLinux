be5eb917f7183b9f6465fcafc8623b1b7778e277
Sanyam Agarwal <agarwalsanyam35@gmail.com>
2025-06-25 23:10:50 +0530

    Update and rename RAM_STATUS to RAM_STATUS.sh
    Added comment as well

--- RAM_STATUS.sh
+++ RAM_STATUS.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This Script will let us know the free RAM status. we can also create cronjob of this and will send automatic mail to our system if it will come below threshold
 
 FREE_SPACE=$(free -mt | grep "Total" | awk '{print $4}')
 TH=4200
